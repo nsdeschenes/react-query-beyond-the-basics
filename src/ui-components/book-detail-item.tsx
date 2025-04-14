f240420d0a9d0faa09e794939c492d6a06edcd20
TkDodo <office@dorfmeister.cc>
2025-04-14 16:02:22 +0200

    markdown parsing

--- src/ui-components/book-detail-item.tsx
+++ src/ui-components/book-detail-item.tsx
@@ -1,3 +1,5 @@
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import { CoverImage } from '@/ui-components/cover-image'
 
 type Props = {
@@ -38,30 +40,34 @@ export function BookDetailItem({
           </div>
         )}
 
-        <p className="mb-4 whitespace-pre-line text-gray-300">{description}</p>
-
-        {links && links.length > 0 && (
-          <div className="mt-6">
-            <h3 className="mb-2 text-lg font-semibold text-white">
-              Related Links
-            </h3>
-            <ul className="space-y-2">
-              {links.map((link) => (
-                <li key={link.url}>
-                  <a
-                    href={link.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-indigo-400 hover:underline"
-                  >
-                    {link.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+        <div className="prose mb-4 max-w-none whitespace-pre-line text-gray-300">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {description.replaceAll(String.raw`\r\n`, '\n')}
+          </ReactMarkdown>
+        </div>
       </div>
+
+      {links && links.length > 0 && (
+        <div className="mt-6">
+          <h3 className="mb-2 text-lg font-semibold text-white">
+            Related Links
+          </h3>
+          <ul className="space-y-2">
+            {links.map((link) => (
+              <li key={link.url}>
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-indigo-400 hover:underline"
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
