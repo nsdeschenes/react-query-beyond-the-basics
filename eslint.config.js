095947cac1be9314a437f11a03dbee824c04b10f
TkDodo <office@dorfmeister.cc>
2025-04-14 13:11:16 +0200

    eslint-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import eslintReact from '@eslint-react/eslint-plugin'
 import pluginQuery from '@tanstack/eslint-plugin-query'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
@@ -8,8 +10,12 @@ export default tseslint.config(
   tseslint.configs.strictTypeChecked,
   pluginQuery.configs['flat/recommended'],
   eslintPluginUnicorn.configs.recommended,
+  eslintReact.configs['recommended-type-checked'],
+  reactHooks.configs['recommended-latest'],
   {
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
